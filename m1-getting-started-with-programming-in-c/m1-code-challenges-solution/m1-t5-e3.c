276bc80b6d94b5bdffd805712eb13acaee30ec5f
siva200 <66561566+siva200@users.noreply.github.com>
2020-06-07 20:28:40 +0530

    m1-t3-e5.c

--- m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t5-e3.c
+++ m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t5-e3.c
@@ -4,6 +4,11 @@
 
         puts("I am Rahul.");
         puts("I am 20 years old.");
+        printf("I am Siva ranjini.\n");
+        printf("I am 19 years old.\n");
+        puts("I am Siva ranjini.");
+        puts("I am 19 years old.");
+        
 
         return 0;
     }

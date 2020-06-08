613c608a821fa9ec2aee93cd07dc10b192c8d063
siva200 <66561566+siva200@users.noreply.github.com>
2020-06-08 10:35:33 +0530

    m1-t6-e6.c

--- m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t6-e3.c
+++ m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t6-e3.c
@@ -6,6 +6,10 @@ int main() {
     char dollar = '$';
     float gdp = 2.2;
     int year = 2015;
+    printf("%s",name); 
+    printf("%c",dollar);
+    printf("%.1f",God); 
+    printf("%d",year); 
 
     printf("As economic reforms picked up the pace, %s's GDP grew five-fold to reach US%c%.1f trillion in %d (as per IMF estimates).", name, dollar, gdp, year);
 

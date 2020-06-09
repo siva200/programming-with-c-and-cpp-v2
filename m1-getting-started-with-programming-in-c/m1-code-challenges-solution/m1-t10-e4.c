b7f04db896fba95d81de32f741976bf7d7a254f0
siva200 <66561566+siva200@users.noreply.github.com>
2020-06-09 12:29:23 +0530

    m1-t10-e4.c

--- m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t10-e4.c
+++ m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t10-e4.c
@@ -13,15 +13,22 @@ int main() {
     puts("Enter bill: ");    // User input 
     scanf("%f", &bill);
 
-    tax = bill * 5/100;    // Calculate tax
+    tax = bill * 5/100;
+    // Calculate tax
+     tax=1500 * 5/100;
 
     totalBill = bill + tax;    // Calculate total bill 
-
+     totalbill = 1500 + 75
     // If the total bill is greater than or equal to Rs. 1000 then apply a discount. 
-    if (totalBill >= 1000) {
+    if (totalBill >= 1000) 
+    if(1575 >= 1000)
+    {
         netPayableAmount = totalBill * 0.9;
+        netPayableAmount = 1575 * 0.9;
         printf("Net payable amount after discount: %.2f", netPayableAmount);
-    } else {     // Else don’t apply for a discount. 
+    } else 
+      else
+     {     // Else donâ€™t apply for a discount. 
         netPayableAmount = totalBill;
         printf("Net payable amount: %.2f", netPayableAmount);
     }
